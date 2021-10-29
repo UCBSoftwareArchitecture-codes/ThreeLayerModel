ea94b19f05eed3dfda34d74a39f176fdf9be64c0
Pablo Sangueza <pablo.sangueza@niceincontact.com>
2021-10-29 10:17:15 -0400

    Add structure of project

---
+++ ThreeLayerModel.BusinessLogic.Test/UnitTest1.cs
@@ -0,0 +1,18 @@
+using NUnit.Framework;
+
+namespace ThreeLayerModel.BusinessLogic.Test
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
